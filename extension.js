ef1a302021b70543f50b6aebc1ba0e3432ae7d55
De Blaze Media <blazemedia29@gmail.com>
2025-06-06 17:16:10 +0530

    new yt videos

--- extension.js
+++
@@ -1,5 +0,0 @@
-// Hide .html extension using JS redirect (only for demo/testing)
-// if (window.location.pathname.endsWith(".html")) {
-//     const newUrl = window.location.pathname.replace(".html", "");
-//     window.history.replaceState({}, "", newUrl);
-// }
